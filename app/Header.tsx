a65735ff1d3a8d5132e6de2215edba0e729ad76f
Anelia <aneliatahibet02@gmail.com>
2025-03-02 23:06:20 +0100

    shop2

--- app/Header.tsx
+++ app/Header.tsx
@@ -7,11 +7,11 @@ import avatar from '@/public/assets/images/8.jpg'
 
 export default function Header() {
   return (
-    <div className="px-56" >
+    <div className="px-56 max-w-screen-2xl mx-auto" >
     <header className="flex justify-between border-b-2 border-gray-200 ">
      <div className=" flex gap-8  items-start">
       <Image src={logo} alt="logo"/>
-     <nav className="flex gap-4 text-gray-400">
+     <nav className="flex gap-4 text-gray-400 group-hidden">
       <a href="">Collections</a>
       <a href="">Men</a>
       <a href="">Women</a>
